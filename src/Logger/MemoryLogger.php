edace1d7fb1d68d9e16cd01951599776ce472a90
tiananwei <awei.tian@qq.com>
2017-06-29 16:19:26 +0800

    经过实际使用，FP使用实例变量更好

--- src/Logger/MemoryLogger.php
+++ src/Logger/MemoryLogger.php
@@ -3,7 +3,7 @@
 namespace Tian\Logger;
 
 class MemoryLogger extends \Psr\Log\AbstractLogger {
-	private static $fp = NULL;
+	private $fp = NULL;
 	/**
 	 * Logs with an arbitrary level.
 	 *
@@ -14,25 +14,34 @@ class MemoryLogger extends \Psr\Log\AbstractLogger {
 	 * @return void
 	 */
 	public function log($level, $message, array $context = array()) {
-		if (is_null ( self::$fp )) {
-			self::$fp = fopen ( "php://memory", 'r+' );
+		if (is_null ( $this->fp )) {
+			$this->fp = fopen ( "php://memory", 'r+' );
 		}
-		fputs ( self::$fp, $level . ': ' . strtr ( $message, $context ) . "\n" );
+		fputs ( $this->fp, $level . ': ' . strtr ( $message, $context ) . "\n" );
 	}
 	/**
 	 *
 	 * @return string
 	 */
 	public function getLog() {
-		if (is_null ( self::$fp )) {
+		if (is_null ( $this->fp )) {
 			return '';
 		}
-		rewind ( self::$fp );
+		rewind ( $this->fp );
 		$str = '';
-		while ( ! feof ( self::$fp ) ) {
-			$str .= fread ( self::$fp, 1024 );
+		while ( ! feof ( $this->fp ) ) {
+			$str .= fread ( $this->fp, 1024 );
 		}
-		fclose ( self::$fp );
 		return $str;
+	}
+	public function clear() {
+		if (! is_null ( $this->fp )) {
+			rewind ( $this->fp );
+		}
+	}
+	public function close() {
+		if (! is_null ( $this->fp )) {
+			fclose ( $this->fp );
+		}
 	}
 }
